7c5b591eecb42a345933de8b0492c50fe62ab8c8
Michael Kirk <michael.john.kirk@gmail.com>
2015-01-30 21:06:32 -0800

    fix-up configure environment variables.

--- tests/scripts/osx/configure.sh
+++ tests/scripts/osx/configure.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+export QGIS_SRC_DIRECTORY=/Users/mkirk/src/qgis
+export QGIS_BUILD_DIRECTORY=/Users/mkirk/src/qgis/build
+export HOMEBREW_PREFIX=`brew --prefix`
+export DYLD_LIBRARY_PATH=${QGIS_BUILD}/output/lib:${QGIS_BUILD}/PlugIns/qgis
+export PATH=${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin::/usr/X11/bin
+export PYTHONHOME=${HOMEBREW_PREFIX}/Frameworks/Python.framework/Versions/2.7
+export PYTHONPATH=${HOMEBREW_PREFIX}/lib/python2.7/site-packages
+export PYQGIS_STARTUP=${HOMEBREW_PREFIX}/Library/Taps/osgeo/homebrew-osgeo4mac/enviro/python_startup.py
+export GDAL_DRIVER_PATH=${HOMEBREW_PREFIX}/lib/gdalplugins
+
 mkdir -p build
 cd build
 cmake .. -DCXX_EXTRA_FLAGS="-I'/usr/local/opt/gettext/include' -F/usr/local/opt/qt/lib -isystem/usr/local/include -isystem/opt/X11/include -isystem/opt/X11/include/freetype2 -isystem/System/Library/Frameworks/OpenGL.framework/Versions/Current/Headers -I/usr/local/opt/readline/include -I/usr/local/opt/sqlite/include -I/usr/local/opt/expat/include -I/usr/local/opt/openssl/include -I/usr/local/opt/libxml2/include -I/usr/local/opt/liblwgeom/include -I/usr/local/opt/gettext/include -I/usr/local/opt/icu4c/include -L/usr/local/opt/bison/lib -L/usr/local/opt/readline/lib -L/usr/local/opt/sqlite/lib -L/usr/local/opt/expat/lib -L/usr/local/opt/openssl/lib -L/usr/local/opt/libxml2/lib -L/usr/local/opt/liblwgeom/lib -L/usr/local/opt/gettext/lib -L/usr/local/opt/libffi/lib -L/usr/local/opt/icu4c/lib -L/usr/local/lib -L/opt/X11/lib -L/System/Library/Frameworks/OpenGL.framework/Versions/Current/Libraries -Wl,-headerpad_max_install_names"\
@@ -18,7 +28,7 @@ cmake .. -DCXX_EXTRA_FLAGS="-I'/usr/local/opt/gettext/include' -F/usr/local/opt/
   -DQSCI_SIP_DIR=/usr/local/opt/qscintilla2/share/sip \
   -DWITH_INTERNAL_QWTPOLAR=FALSE \
   -DQGIS_MACAPP_BUNDLE=0 \
-  -DQGIS_MACAPP_DEV_PREFIX='/Users/mkirk/src/qgis/build/artifacts/lib/qgis-dev' \
+  -DQGIS_MACAPP_DEV_PREFIX="$QGIS_BUILD_DIRECTORY/lib/qgis-dev" \
   -DQGIS_MACAPP_INSTALL_DEV=TRUE \
   -DWITH_QSCIAPI=FALSE \
   -DWITH_STAGED_PLUGINS=FALSE \
@@ -31,8 +41,7 @@ cmake .. -DCXX_EXTRA_FLAGS="-I'/usr/local/opt/gettext/include' -F/usr/local/opt/
   -DFCGI_LIBRARY=/usr/local/opt/fcgi/lib/libfcgi.dylib \
   -DPOSTGRES_CONFIG=/usr/local/opt/postgresql/bin/pg_config \
   -DWITH_GRASS=FALSE \
-  -DGRASS_PREFIX='/usr/local/opt/grass-64/grass-6.4.4' \
-  -DWITH_GLOBE=TRUE \
+  -DWITH_GLOBE=FALSE \
   -DOSG_PLUGINS_PATH=/usr/local/lib/osgPlugins-3.2.0 \
   -DWITH_ORACLE=FALSE \
   -DWITH_APIDOC=FALSE
