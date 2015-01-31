c5bdabd79eef7b8ba7e83c121e220dd15d9949e6
Michael Kirk <michael.john.kirk@gmail.com>
2015-01-30 17:07:47 -0800

    another attempt to circumnavigate test hang by not building the extra featurs

--- tests/scripts/osx/setup.sh
+++ tests/scripts/osx/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 export HOMEBREW_PREFIX=`brew --prefix`
 brew update
-brew install open-scene-graph
-# brew install python
-# brew tap homebrew/science
-# brew tap osgeo/osgeo4mac
-# brew install qgis-26 --only-dependencies --with-grass --with-globe
+brew install python
+brew tap homebrew/science
+brew tap osgeo/osgeo4mac
+brew install qgis-26 --only-dependencies
