5b73d50bd8329b63dca4a1ba2502eb02a95e5237
Michael Kirk <michael.john.kirk@gmail.com>
2015-02-02 17:30:40 -0800

    explicitly install pyqt before qgis

--- tests/scripts/osx/setup.sh
+++ tests/scripts/osx/setup.sh
@@ -6,6 +6,7 @@ sysctl hw.ncpu
 export HOMEBREW_PREFIX=`brew --prefix`
 brew update
 brew install python
+brew install pyqt
 brew tap homebrew/science
 brew tap osgeo/osgeo4mac
 brew install qgis --only-dependencies --without-globe --without-grass
