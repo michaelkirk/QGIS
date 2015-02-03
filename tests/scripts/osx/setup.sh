ed5aa3395e9335b7541dc312685b29dadcc2d6db
Michael Kirk <michael.john.kirk@gmail.com>
2015-02-02 17:39:32 -0800

    include python path for pyqt

--- tests/scripts/osx/setup.sh
+++ tests/scripts/osx/setup.sh
@@ -7,6 +7,7 @@ export HOMEBREW_PREFIX=`brew --prefix`
 brew update
 brew install python
 brew install pyqt
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
 brew tap homebrew/science
 brew tap osgeo/osgeo4mac
 brew install qgis --only-dependencies --without-globe --without-grass
